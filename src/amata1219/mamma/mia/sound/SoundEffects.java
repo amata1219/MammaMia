75f8536e5412df6d24684e8f043e5ec0ad75148b
amata1219 <suzukiamata@gmail.com>
2021-03-22 00:44:32 +0900

    SEを追加

--- src/amata1219/mamma/mia/sound/SoundEffects.java
+++ src/amata1219/mamma/mia/sound/SoundEffects.java
@@ -4,6 +4,14 @@ import static org.bukkit.Sound.*;
 
 public class SoundEffects {
 
+    public static final SoundEffect OPERATED = new SoundEffect(ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 2.0f);
+
+    public static final SoundEffect PREPARED = new SoundEffect(ENTITY_PLAYER_LEVELUP, 1f, 1.2f);
+
     public static final SoundEffect SUCCEEDED = new SoundEffect(ENTITY_PLAYER_LEVELUP, 1f, 1.8f);
+
+    public static final SoundEffect CANCEL = new SoundEffect(BLOCK_CHAIN_BREAK, 1f, 1.8f);
+
+    public static final SoundEffect FAILED = new SoundEffect(BLOCK_ANVIL_PLACE, 0.7f, 1f);
 
 }
