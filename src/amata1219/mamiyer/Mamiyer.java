c1b9ea5c3d2d16c01a76c6ec7e46bab30f5d794f
amata1219 <suzukiamata@gmail.com>
2021-03-21 15:47:32 +0900

    BukkitコマンドフレームワークBryionakeを追加

---
+++ src/amata1219/mamiyer/Mamiyer.java
@@ -0,0 +1,6 @@
+package amata1219.mamiyer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mamiyer extends JavaPlugin {
+}
---
+++ src/amata1219/mamiyer/bryionake/constant/CommandSenderCasters.java
@@ -0,0 +1,11 @@
+package amata1219.mamiyer.bryionake.constant;
+
+import amata1219.mamiyer.bryionake.dsl.caster.SafeCaster;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandSenderCasters {
+
+    public static final SafeCaster<CommandSender, Player, String> casterToPlayer = new SafeCaster<>(() -> Constants.ERROR_MESSAGE_PREFIX + "ゲーム内から実行して下さい。");
+
+}
