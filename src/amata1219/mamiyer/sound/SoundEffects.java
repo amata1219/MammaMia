cde5eaa4996dae12da0e7b46b3e4ac06145bb2bb
amata1219 <suzukiamata@gmail.com>
2021-03-21 16:00:35 +0900

    アクションの成功時に再生するSEを追加

---
+++ src/amata1219/mamiyer/sound/SoundEffects.java
@@ -0,0 +1,9 @@
+package amata1219.mamiyer.sound;
+
+import static org.bukkit.Sound.*;
+
+public class SoundEffects {
+
+    public static final SoundEffect SUCCEEDED = new SoundEffect(ENTITY_PLAYER_LEVELUP, 1f, 1.8f);
+
+}
