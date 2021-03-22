6bb23cb9c8b9c0cf25bd1aff472edd49839584a4
amata1219 <suzukiamata@gmail.com>
2021-03-22 11:34:11 +0900

    クリックでボートを水上に召喚出来る機能を追加

--- src/amata1219/mamma/mia/listener/TemporaryBoatListener.java
+++ src/amata1219/mamma/mia/listener/TemporaryBoatListener.java
@@ -2,21 +2,21 @@ package amata1219.mamma.mia.listener;
 
 import amata1219.mamma.mia.MammaMia;
 import amata1219.mamma.mia.config.section.TemporaryBoatSection;
+import amata1219.mamma.mia.reflect.APIPackage;
 import com.google.common.collect.ImmutableSet;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.TreeSpecies;
+import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Boat;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Vehicle;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.joor.Reflect;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,16 +31,20 @@ public class TemporaryBoatListener implements Listener {
 
     private final MammaMia plugin = MammaMia.instance();
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void on(PlayerInteractEvent event) {
         TemporaryBoatSection section = plugin.config().temporaryBoatSection();
-        if (!section.enabledOrNot() || event.getAction() != Action.RIGHT_CLICK_BLOCK || event.isBlockInHand()) return;
+        Action action = event.getAction();
+        if (!section.enabledOrNot() || (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) || event.isBlockInHand()) return;
 
         Player player = event.getPlayer();
         if (player.isInsideVehicle() || player.isSneaking() || player.hasMetadata("race") || player.hasMetadata("race-team")) return;
 
         Block block = event.getClickedBlock();
-        if (!block.getType().name().endsWith("ICE")) return;
+        if (block != null)
+            if (!block.getType().name().endsWith("ICE")) return;
+        else
+            if ((block = waterClicked(player.getWorld(), player, event.getHand())) == null) return;
 
         if (event.getMaterial().name().endsWith("_BOAT")) return;
 
@@ -67,6 +71,79 @@ public class TemporaryBoatListener implements Listener {
         if (!(vehicle instanceof Boat)) return;
 
         if (vehicle.hasMetadata(TEMPORARY_BOAT_METADATA)) vehicle.remove();
+    }
+
+    private static Block waterClicked(World world, Player player, EquipmentSlot handType) {
+        Object nmsWorld = Reflect.on(world)
+                .call("getHandle")
+                .get();
+        Object entityHuman = Reflect.on(player)
+                .call("getHandle")
+                .get();
+        Object fluidCollisionOption = Reflect.onClass(APIPackage.NET_MINECRAFT_SERVER + "RayTrace.FluidCollisionOption")
+                .get("ANY");
+
+        Object blockClicked = Reflect.onClass(APIPackage.NET_MINECRAFT_SERVER + "Item")
+                .call("a", nmsWorld, entityHuman, fluidCollisionOption)
+                .get();
+
+        Object clickedBlockType = Reflect.on(blockClicked)
+                .call("getType")
+                .get();
+        Object movingObjectType = Reflect.onClass(APIPackage.NET_MINECRAFT_SERVER + "MovingObjectPosition.EnumMovingObjectType")
+                .get("BLOCK");
+
+        if (clickedBlockType != movingObjectType) return null;
+
+        Object blockPosition = Reflect.on(blockClicked)
+                .call("getBlockPosition")
+                .get();
+        Object blockFace = Reflect.on(blockClicked)
+                .call("getDirection")
+                .get();
+        Object adjacentBlockPosition = Reflect.on(blockPosition)
+                .call("shift", blockFace)
+                .get();
+        Object itemInHand = Reflect.on(entityHuman)
+                .call("b", equipmentSlotToEnumHand(handType))
+                .get();
+
+        boolean result1 = Reflect.on(nmsWorld)
+                .call("a", entityHuman, blockPosition)
+                .get();
+        boolean result2 = Reflect.on(entityHuman)
+                .call("a", adjacentBlockPosition, blockFace, itemInHand)
+                .get();
+
+        if (!(result1 && result2)) return null;
+
+        boolean isFluid = Reflect.on(nmsWorld)
+                .call("getType", blockPosition)
+                .call("getFluid")
+                .call("isEmpty")
+                .get();
+
+        if (!isFluid) return null;
+
+        int x = Reflect.on(blockPosition)
+                .call("getX")
+                .get();
+        int y = Reflect.on(blockPosition)
+                .call("getY")
+                .get();
+        int z = Reflect.on(blockPosition)
+                .call("getZ")
+                .get();
+        return world.getBlockAt(x, y, z);
+    }
+
+    private static Object equipmentSlotToEnumHand(EquipmentSlot handType) {
+        Reflect reflect = Reflect.onClass(APIPackage.NET_MINECRAFT_SERVER + "EnumHand");
+        switch (handType) {
+            case HAND: return reflect.get("MAIN_HAND");
+            case OFF_HAND: return reflect.get("OFF_HAND");
+            default: throw new IllegalArgumentException("handType must be EquipmentSlot.HAND or EquipmentSlot.OFF_HAND");
+        }
     }
 
 }
